3c686d8cac0aba3135ecf9d3e49ab19e86f2db2a
Steelnox <eric14pastor@gmail.com>
2020-12-23 10:40:19 +0100

    Ejercicio 1 Hecho

---
+++ UD3_Ex1/UD3_Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UD3_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            int y = 6;
+
+            Console.WriteLine("Suma: {0}", x + y);
+            Console.WriteLine("Resta: {0}", x - y);
+            Console.WriteLine("Multiplicación: {0}", x * y);
+            Console.WriteLine("División: {0}", x / y);
+            Console.WriteLine("Modulo: {0}", x % y);
+        }
+    }
+}
