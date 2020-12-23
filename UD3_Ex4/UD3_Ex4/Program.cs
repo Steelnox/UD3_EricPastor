cfbf06a5a37a3482f41e978f8caf7b8205382bcd
Steelnox <eric14pastor@gmail.com>
2020-12-23 11:26:13 +0100

    Ejercicio 4

---
+++ UD3_Ex4/UD3_Ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD3_Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 2;
+            Console.WriteLine("{0} + 77 = {1}", N, N += 77);
+            Console.WriteLine("{0} - 3 = {1}", N, N -= 3);
+            Console.WriteLine("{0} * 2 = {1}", N, N *= 2);
+        }
+    }
+}
