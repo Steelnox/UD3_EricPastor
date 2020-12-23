7e584dbac93e32b1bc94dee617d20bf61f9f33b7
Steelnox <eric14pastor@gmail.com>
2020-12-23 11:01:18 +0100

    Ejercicio 2

---
+++ UD3_Ex2/UD3_Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UD3_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            double A = 200;
+            char C = 'b';
+
+            Console.WriteLine("Variable N = {0}", N);
+            Console.WriteLine("Variable A = {0}", A);
+            Console.WriteLine("Variable C = {0}", C);
+
+            Console.WriteLine("{0} + {1} = {2}", N, A, N + A);
+            Console.WriteLine("{0} - {1} = {2}", A, N, A - N);
+            Console.WriteLine("Valor numérico del carácter {0} = {1}", C, (int)C);
+
+        }
+    }
+}
